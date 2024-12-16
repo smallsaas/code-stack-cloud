254c9a507846f24460298fc93259d95dc3d21257
kequandian <kequandian>
2024-12-16 23:03:10 +0800

    fix mbcs

--- src/main/resources/sql/meta-entity-patch-machine-schema.sql
+++ src/main/resources/sql/meta-entity-patch-machine-schema.sql
@@ -5,9 +5,10 @@ CREATE TABLE `meta_entity_patch_machine` (
   `entity` varchar(64) NOT NULL COMMENT '实体名称',
   `entity_table_name` varchar(64) NOT NULL COMMENT '实体对应表名',
   `entity_field_name` varchar(64) NOT NULL COMMENT '实体字段名',
-  `entity_field_type` varchar(16) NOT NULL DEFAULT 'STRING' COMMENT '实体字段类型',
-  `number_range_min` bigint(20) DEFAULT NULL COMMENT '数字类型字段的范围下限',
-  `number_range_max` bigint(20) DEFAULT NULL COMMENT '数字类型字段的范围上限',
+  `entity_field_type` varchar(16) NOT NULL DEFAULT 'STRING' COMMENT '实体字段类型[STRING, NUMBER, DATE]',
+  `where_field_name` varchar(64) DEFAULT NULL COMMENT '实体类查询条件字段',
+  `number_range_min` int(11) DEFAULT NULL COMMENT '数字类型字段的范围下限',
+  `number_range_max` int(11) DEFAULT NULL COMMENT '数字类型字段的范围上限',
   `permission` varchar(64) DEFAULT NULL COMMENT '操作权限控制',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_meta_patch_entity` (`entity`,`entity_field_name`)
